bffc39cfb1cd059a5e935bf0f715b4bd1929608c
unknown <leandromanuel291295@gmail.com>
2022-08-16 19:39:44 -0300

    primer comit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express= require("express");
+const path= require("path");
+const app= express();
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.listen(3000, () => console.log("Servidor corriendo"));
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+});
