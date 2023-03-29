5c9a06cf20ca38d7b0ecebafae7b5d8cea098fb4
Nberisso <nberisso11@gmail.com>
2023-03-29 12:23:57 +0200

    Deploy setup

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "blended_cafe",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
   "test": {
@@ -14,10 +17,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
